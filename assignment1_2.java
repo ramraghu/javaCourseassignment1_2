e67cee29ebe9324e9948fb6e402d281a39f591df
ramraghu <raghuramnair2015@gmail.com>
2016-09-26 01:53:19 +0530

    assignment1_2

---
+++ assignment1_2.java
@@ -0,0 +1,19 @@
+package learnJava;
+
+public class assignment1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20; 
+		int b = 10; 
+		int c = a-- - --a;
+		System.out.println(c);
+		c=a--;
+		System.out.println(c);
+		int d=a>>2;
+		System.out.println(d);
+		int e=a&b;
+		System.out.println(e);
+}
+}
+
